0fa2f32cdaecfb5bd45446dc7ad185a071ecc558
Tom Kester <kes19002@byui.edu>
2025-10-22 10:20:12 -0600

    week 4 personal - swagger docs

--- personal/routes/routes.js
+++ personal/routes/routes.js
@@ -4,5 +4,9 @@ const { home, lookup } = require('../controllers/maincontroller')
 routes.get('/', home)
 routes.get('/lookup', lookup)
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger-output.json');
+routes.use('/api-docs', swaggerUi.serve);
+routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = routes
---
+++ personal/swagger.js
@@ -0,0 +1,19 @@
+const swaggerAutogen = require('swagger-autogen')();
+const environment = require("dotenv").config();
+const HOST_PATH = process.env.HOST_PATH;
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Description'
+  },
+  host: HOST_PATH + '/contacts'
+};
+
+const outputFile = './swagger-output.json';
+const routes = ['./routes/contacts.js'];
+
+/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
+root file where the route starts, such as index.js, app.js, routes.js, etc ... */
+
+swaggerAutogen(outputFile, routes, doc);
